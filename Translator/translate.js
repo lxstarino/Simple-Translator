77ee192491cff48a3fca1d048c247a22905d68a4
lxst <68613828+lxstarino@users.noreply.github.com>
2021-03-16 15:16:41 +0100

    Add files via upload

---
+++ Translator/translate.js
@@ -0,0 +1,26 @@
+const textInput = document.getElementById("textInput");
+const textOutput = document.getElementById("textOutput");
+
+textInput.addEventListener("keyup", (e) => {
+  const input = e.target.value;
+  const output = input
+    .split(" ")
+    .map((word) => 
+    {
+        switch(word)
+        {
+            case "one":
+                word = "eins";
+                break;
+            case "two":
+                word = "zwei";
+                break;
+            case "three":
+                word = "drei";
+                break;
+        }
+        return (word);
+    })
+    .join(" ");
+     textOutput.innerText = output;
+});
